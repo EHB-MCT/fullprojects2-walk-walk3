0b2a2f76f4827d4fec22aef9b546a6154336f4b5
LarsChokier <lars.chokier@student.ehb.be>
2026-06-08 16:25:43 +0200

    Made page 1

--- javascript/Javascript.js
+++ javascript/Javascript.js
@@ -13,3 +13,30 @@ toggle.addEventListener("change", function () {
     fr.classList.remove("wit");
   }
 });
+
+var inputs = document.querySelectorAll(".locatieObject input");
+var kaart = document.getElementById("kaart");
+var openMapLink = document.getElementById("openMapLink");
+
+function updateMap() {
+  var straat = document.getElementById("inputStraat").value;
+  var gemeente = document.getElementById("inputGemeente").value;
+  var postcode = document.getElementById("inputPostcode").value;
+  var adres = straat + " " + postcode + " " + gemeente + " Brussel";
+  var embedUrl =
+    "https://maps.google.com/maps?q=" +
+    encodeURIComponent(adres) +
+    "&output=embed";
+  var fullUrl = "https://maps.google.com/maps?q=" + encodeURIComponent(adres);
+
+  if (kaart) {
+    kaart.src = embedUrl;
+  }
+  if (openMapLink) {
+    openMapLink.href = fullUrl;
+  }
+}
+
+inputs.forEach(function (input) {
+  input.addEventListener("input", updateMap);
+});
