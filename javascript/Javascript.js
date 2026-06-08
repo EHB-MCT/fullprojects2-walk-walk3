593ec32b7ba748420bd18d925bce02a786fd2de2
LarsChokier <lars.chokier@student.ehb.be>
2026-06-08 12:20:23 +0200

    Made the navigation and started with the first page

--- javascript/Javascript.js
+++ javascript/Javascript.js
@@ -1,1 +1,15 @@
+var toggle = document.getElementById("taalToggle");
+var nl = document.querySelector(".taal-nl");
+var fr = document.querySelector(".taal-fr");
 
+nl.classList.add("wit");
+
+toggle.addEventListener("change", function () {
+  if (toggle.checked) {
+    nl.classList.remove("wit");
+    fr.classList.add("wit");
+  } else {
+    nl.classList.add("wit");
+    fr.classList.remove("wit");
+  }
+});
