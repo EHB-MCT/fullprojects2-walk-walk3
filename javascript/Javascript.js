eb1b7d53df6bf6a9eb8da4e4606d7b96bb0e9fc3
LarsChokier <lars.chokier@student.ehb.be>
2026-06-11 13:34:54 +0200

    added location button functionality and fixed some buttons

--- javascript/Javascript.js
+++ javascript/Javascript.js
@@ -14,9 +14,8 @@ toggle.addEventListener("change", function () {
   }
 });
 
-var inputs = document.querySelectorAll(".locatieObject input");
-var kaart = document.getElementById("kaart");
-var openMapLink = document.getElementById("openMapLink");
+var kaartMobile = document.getElementById("kaartMobile");
+var kaartDesktop = document.getElementById("kaartDesktop");
 
 function updateMap() {
   var straat = document.getElementById("inputStraat").value;
@@ -27,33 +26,76 @@ function updateMap() {
     "https://maps.google.com/maps?q=" +
     encodeURIComponent(adres) +
     "&output=embed";
-  var fullUrl = "https://maps.google.com/maps?q=" + encodeURIComponent(adres);
 
-  if (kaart) {
-    kaart.src = embedUrl;
-  }
-  if (openMapLink) {
-    openMapLink.href = fullUrl;
-  }
+  if (kaartMobile) kaartMobile.src = embedUrl;
+  if (kaartDesktop) kaartDesktop.src = embedUrl;
 }
 
-inputs.forEach(function (input) {
-  input.addEventListener("input", updateMap);
-});
+var inputs = document.querySelectorAll(".locatieObject input");
+if (inputs.length > 0) {
+  inputs.forEach(function (input) {
+    input.addEventListener("input", updateMap);
+  });
+}
 
-const inputFoto = document.getElementById("inputFoto");
-const fotoPreview = document.getElementById("fotoPreview");
-const fotoLabelText = document.getElementById("fotoLabelText");
+var inputFoto = document.getElementById("inputFoto");
+if (inputFoto) {
+  var fotoPreview = document.getElementById("fotoPreview");
+  var fotoLabelText = document.getElementById("fotoLabelText");
+  inputFoto.addEventListener("change", function () {
+    var file = this.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        fotoPreview.src = e.target.result;
+        fotoPreview.style.display = "block";
+        fotoLabelText.style.display = "none";
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+}
 
-inputFoto.addEventListener("change", function () {
-  const file = this.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      fotoPreview.src = e.target.result;
-      fotoPreview.style.display = "block";
-      fotoLabelText.style.display = "none";
-    };
-    reader.readAsDataURL(file);
-  }
+document.querySelectorAll(".deelLocatieButton").forEach(function (knop) {
+  knop.addEventListener("click", function () {
+    navigator.geolocation.getCurrentPosition(function (positie) {
+      var lat = positie.coords.latitude;
+      var lon = positie.coords.longitude;
+      fetch(
+        "https://nominatim.openstreetmap.org/reverse?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&format=json",
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var adres = data.address;
+          var straat = (adres.road || "") + " " + (adres.house_number || "");
+          var gemeente = adres.city || adres.town || adres.municipality || "";
+          var postcode = adres.postcode || "";
+
+          document.getElementById("inputStraat").value = straat;
+          document.getElementById("inputGemeente").value = gemeente;
+          document.getElementById("inputPostcode").value = postcode;
+
+          if (document.getElementById("inputStraatDesktop")) {
+            document.getElementById("inputStraatDesktop").value = straat;
+            document.getElementById("inputGemeenteDesktop").value = gemeente;
+            document.getElementById("inputPostcodeDesktop").value = postcode;
+          }
+
+          var volledigAdres =
+            straat + " " + postcode + " " + gemeente + " Brussel";
+          var embedUrl =
+            "https://maps.google.com/maps?q=" +
+            encodeURIComponent(volledigAdres) +
+            "&output=embed";
+          if (kaartMobile) kaartMobile.src = embedUrl;
+          if (kaartDesktop) kaartDesktop.src = embedUrl;
+        });
+    });
+  });
 });
